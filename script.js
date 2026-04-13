180e7ffdc6ba3f420db4cf98eecf3bb758ff274d
nguyentuanthanhdmxah-dotcom <nguyentuanthanhdmxah@gmail.com>
2026-04-13 22:36:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const data = [
 {name:"iPhone 14 Pro",brand:"iphone",img:"https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-pro.jpg",price:27990000,desc:"Camera 48MP",ram:"6GB",rom:"128GB",screen:"6.1 OLED"},
 {name:"iPhone 13",brand:"iphone",img:"https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13.jpg",price:17990000,desc:"Chip A15",ram:"4GB",rom:"128GB",screen:"6.1 OLED"},
 
-{name:"Samsung S24 Ultra",brand:"samsung",img:"https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s24-ultra-5g.jpg",price:32990000,desc:"Snapdragon 8 Gen 3",ram:"12GB",rom:"256GB",screen:"6.8 AMOLED"},
-{name:"Samsung S23",brand:"samsung",img:"https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s23.jpg",price:21990000,desc:"Flagship",ram:"8GB",rom:"256GB",screen:"6.1 AMOLED"},
+{name:"Samsung S24 Ultra",brand:"samsung",img:"https://tse1.mm.bing.net/th/id/OIP.FpxLz2z3K0uTLDDu0ZNBMgHaHc?rs=1&pid=ImgDetMain&o=7&rm=3",price:32990000,desc:"Snapdragon 8 Gen 3",ram:"12GB",rom:"256GB",screen:"6.8 AMOLED"},
+{name:"Samsung S23",brand:"samsung",img:"https://tse1.mm.bing.net/th/id/OIP.lDrmGuecyU04XjjB_UGyZQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",price:21990000,desc:"Flagship",ram:"8GB",rom:"256GB",screen:"6.1 AMOLED"},
 {name:"Samsung A54",brand:"samsung",img:"https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a54.jpg",price:8990000,desc:"Pin trâu",ram:"8GB",rom:"128GB",screen:"6.4 AMOLED"},
 
 {name:"Xiaomi 14",brand:"xiaomi",img:"https://fdn2.gsmarena.com/vv/bigpic/xiaomi-14.jpg",price:19990000,desc:"Leica",ram:"12GB",rom:"256GB",screen:"6.36 AMOLED"},
-{name:"Xiaomi 13",brand:"xiaomi",img:"https://fdn2.gsmarena.com/vv/bigpic/xiaomi-13.jpg",price:15990000,desc:"Ổn định",ram:"8GB",rom:"256GB",screen:"6.36 AMOLED"},
+{name:"Xiaomi 13",brand:"xiaomi",img:"https://tse2.mm.bing.net/th/id/OIP.U4QZu6u41GoesADbuUMJkQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",price:15990000,desc:"Ổn định",ram:"8GB",rom:"256GB",screen:"6.36 AMOLED"},
 
 {name:"Oppo Find X5",brand:"oppo",img:"https://fdn2.gsmarena.com/vv/bigpic/oppo-find-x5.jpg",price:18990000,desc:"Thiết kế đẹp",ram:"8GB",rom:"256GB",screen:"6.55 AMOLED"},
 {name:"Oppo Reno 8",brand:"oppo",img:"https://fdn2.gsmarena.com/vv/bigpic/oppo-reno8.jpg",price:9990000,desc:"Camera đẹp",ram:"8GB",rom:"128GB",screen:"6.4 AMOLED"},
 
 {name:"Vivo V27",brand:"vivo",img:"https://fdn2.gsmarena.com/vv/bigpic/vivo-v27.jpg",price:10990000,desc:"Thời trang",ram:"8GB",rom:"256GB",screen:"6.78 AMOLED"},
-{name:"Realme GT",brand:"realme",img:"https://fdn2.gsmarena.com/vv/bigpic/realme-gt.jpg",price:8990000,desc:"Gaming",ram:"8GB",rom:"128GB",screen:"6.43 AMOLED"}
+{name:"Realme GT",brand:"realme",img:"https://tse2.mm.bing.net/th/id/OIP.S6ZK3pkjY0btym-tQLUDLQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",price:8990000,desc:"Gaming",ram:"8GB",rom:"128GB",screen:"6.43 AMOLED"}
 
 ];
 
